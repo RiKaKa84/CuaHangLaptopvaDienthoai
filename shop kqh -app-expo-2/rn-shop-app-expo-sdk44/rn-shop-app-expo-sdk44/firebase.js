3d9d14600a8e7af3cb13d8c9a7d250b69d4088d1
RiKaKa84 <152082734+RiKaKa84@users.noreply.github.com>
2026-05-12 20:24:27 +0700

    Update firebase.js

--- shop kqh -app-expo-2/rn-shop-app-expo-sdk44/rn-shop-app-expo-sdk44/firebase.js
+++ shop kqh -app-expo-2/rn-shop-app-expo-sdk44/rn-shop-app-expo-sdk44/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import config from './config';
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: config.API_KEY,
     authDomain: config.AUTH_DOMAIN,
@@ -16,7 +11,7 @@ const firebaseConfig = {
     appId: config.API_ID,
 };
 
-// Initialize Firebase
+// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
