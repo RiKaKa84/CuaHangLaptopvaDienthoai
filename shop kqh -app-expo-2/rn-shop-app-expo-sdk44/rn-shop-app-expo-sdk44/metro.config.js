9693eb64993ac7bbc48157e9aea18b06550cac17
RiKaKa84 <152082734+RiKaKa84@users.noreply.github.com>
2026-05-12 20:25:12 +0700

    Update metro.config.js

--- shop kqh -app-expo-2/rn-shop-app-expo-sdk44/rn-shop-app-expo-sdk44/metro.config.js
+++ shop kqh -app-expo-2/rn-shop-app-expo-sdk44/rn-shop-app-expo-sdk44/metro.config.js
@@ -2,7 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-// Add support for .cjs files and disable unstable package exports
 defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
 
